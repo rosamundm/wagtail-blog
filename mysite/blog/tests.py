77cc6c0e6832430f47ddaf85a8e340604a7af4d6
rosamund <rsmnd.mthr@gmail.com>
2020-02-02 19:17:14 +0000

    add functional tests

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,9 +1,13 @@
+import os
+from django.test import TestCase
 from wagtail.tests.util import WagtailPageTests
 from blog.models import MyPage
 
-class MyPageTests(WagtailPageTests):
-    def
 
-from django.test import TestCase
+class NewVisitorTest(StaticLiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_server_url = 'http://' + staging_server
 
-# Create your tests here.
